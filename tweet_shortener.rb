3e0c6d8f0163ba976ffe34c218c04759041d04fd
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 19:16:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,3 +11,14 @@ def dictionary(words, substitutes)
 end
 
 def word_subsituter(string)
+  tweet = string.to_a
+  tweet.collect do |x|
+    dictionary_hash.keys.each do |y|
+      if x == y
+        x = dictionary_hash[y]
+        return tweet.to_s
+end
+end
+end    
+
+end
