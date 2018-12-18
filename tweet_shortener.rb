8ff159cdd318356a2301f65b6709f8a5df24f672
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 19:37:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,28 +1,25 @@
-def dictionary
+def dictionary 
   {
-                    "hello" => "hi",
-                    "to" => "2",
-                    "two" => "2",
-                    "too" => "2",
-                    "for" => "4",
-                    "four" => "4",
-                    "be" => "b",
-                    "you" => "u",
-                    "at" => "@",
-                    "and" => "&"
-                  }
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
+}
+
 end
 
-def word_substituter(string)
+def word_substituter(str_tweets)
   result = []
-  dictionary_array = dictionary.keys
-  tweet = string.split(" ")
-  tweet.each do |x|
-      if dictionary_array.include?(x)
-        result << dictionary[x]
-      else
-        result << x
-end
-result.join(" ")
-end
+  arr_dictionary = dictionary.keys
+  arr_tweets = str_tweets.split(' ')
+  arr_tweets.each do |word|
+    arr_dictionary.include?(word) ? result << dictionary[word] : result << word
+  end
+  result.join(' ')
 end
