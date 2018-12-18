d3b0a3e479a736840e7df86050b8740ca870108a
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 20:28:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,3 +32,13 @@ def bulk_tweet_shortener(tweets)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweets)
+    tweets.map do |tweet|
+      if tweet.length > 140
+        word_substituter(tweet)
+      end
+    end
+
+
+end
