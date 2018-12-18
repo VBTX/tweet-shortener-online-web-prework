038454402f5a90949473907454ff9c363f7faf16
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 19:10:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,13 @@
 def dictionary(words, substitutes)
-  hash = {"hello" => "hi"
-  "to, two, too" => "2"
-  "for, four" => "4"
-  "be" => "b"
-  "you" => "u"
-  "at" => "@"
-  "and" => "&"}
+  dictionary_hash = {
+                    "hello" => "hi",
+                    "to, two, too" => "2",
+                    "for, four" => "4",
+                    "be" => "b",
+                    "you" => "u",
+                    "at" => "@",
+                    "and" => "&"
+                  }
 end
+
+def word_subsituter(string)
